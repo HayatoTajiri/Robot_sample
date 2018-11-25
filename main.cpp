548f8e1a1246bad3c65375c1a31a87d2e723630f
HayatoTajiri <40113331+HayatoTajiri@users.noreply.github.com>
2018-11-26 02:05:12 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -40,40 +40,7 @@ DigitalIn swdebug(PC_10); //reserve switch(non connect)
 DigitalIn swkick(PC_12); //reserve switch(non connect)
 
 //declear prototype (function list)
-const float Pi = 3.1415;
-int balldeg(int Deg, int Dis) {
-	int ball = Deg;
-	int Godeg;
-	if (ball >= -45 && ball <= 45) { //ボールが前にあるとき
-		return (2 * ball);
-	} else if ((ball <= -150) && (ball >= 150)) {
-		if (ball >= 0) {
-			Godeg = ball + 90;
-			return (Godeg);
-		} else {
-			Godeg = ball - 90;
-			return (Godeg);
-		}
-	} else {
-		if (Dis <= 400) { //ボールとの距離が近いとき
-			if (ball >= 0) {
-				Godeg = ball
-						+ (180 - 180 / Pi * asin((float) Dis / (float) 400));
-			} else {
-				Godeg = ball
-						- (180 - 180 / Pi * asin((float) Dis / (float) 400));
 
-			}
-		} else {
-			if (ball >= 0) {
-				Godeg = ball + 180 / Pi * asin((float) 400 / (float) Dis); //数値は半径
-			} else {
-				Godeg = ball - 180 / Pi * asin((float) 400 / (float) Dis);
-			}
-		}
-		return (Godeg);
-	}
-}
 
 /*****************************************************************/
 /**********************main function******************************/
@@ -101,35 +68,7 @@ int main() {
 ////////////////Play mode(you can write this statement)////////////
 //***************************************************************//
 		while (sw_start == 1) {
-			imu.get_Euler_Angles(&euler_angles);
-			static int A = 0;
-			A = euler_angles.h;
-			if (A >= 180) {
-				A -= 360;
-			}
-			if (A >= 50) {
-				A = 50;
-			} else if (A <= -50) {
-				A = -50;
-			}
 
-			if (line.direction() != -999) {
-				motor.omniWheels(0,0,0);
-				wait_ms(200);
-				int B = line.direction() - 180;
-				for(int i = 1; i<= 200;i++){
-					motor.omniWheels(B,50,0);
-					wait_ms(1);
-				}
-			} else {
-
-				if (ball.degree() == -999) {
-					motor.omniWheels(0, 0, -0.5*A);
-				} else {
-					motor.omniWheels(balldeg(ball.degree(), ball.distance()),
-							100, -0.5 * A);
-				}
-			}
 		}
 
 //***************************************************************//
